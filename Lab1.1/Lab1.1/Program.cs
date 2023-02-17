034cfa1b09549299925fed97b36893f414d619ba
Andr3ich <106585364+Andr3ich@users.noreply.github.com>
2023-02-17 19:10:47 +0200

    Lab1_second-semester

---
+++ Lab1.1/Lab1.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab1_1
+{
+    public delegate int Delegate(int a, int b);
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Оберiть дiю : +, -, *, /");
+            string a = Console.ReadLine();
+            Console.WriteLine("Перше число : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Друге число :");
+            int y = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case "+":
+                    Delegate Delegate1 = (c, b) => c + b;
+                    int del1 = Delegate1(x, y);
+                    Console.WriteLine("Вiдповiдь : {0}", del1);
+                    break;
+                case "-":
+                    Delegate Delegate2 = (c, b) => c - b;
+                    int del2 = Delegate2(x, y);
+                    Console.WriteLine("Вiдповiдь : {0}", del2);
+                    break;
+                case "*":
+                    Delegate Delegate3 = (c, b) => c * b;
+                    int del3 = Delegate3(x, y);
+                    Console.WriteLine("Вiдповiдь : {0}", del3);
+                    break;
+                case "/":
+                    Delegate Delegate4 = (c, b) => b == 0 ? 0 : c / b;
+                    int del4 = Delegate4(x, y);
+                    Console.WriteLine("Вiдповiдь : {0}", del4);
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab1.2/Lab1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        Func<int>[] delegates = new Func<int>[] {
+            () => rand.Next(0, 10),
+            () => rand.Next(10, 20),
+            () => rand.Next(20, 30)
+        };
+
+        Func<Func<int>[], double> averageDelegateValues = delegate (Func<int>[] dels) {
+            int sum = 0;
+            foreach (Func<int> del in dels)
+            {
+                sum += del();
+            }
+            return (double)sum / dels.Length;
+        };
+
+        double result = averageDelegateValues(delegates);
+        Console.WriteLine("Середнє арифметичне значення: " + result);
+    }
+}
